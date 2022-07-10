9f7e3d45a50c418ac40a9ae369f1edf18885172e
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-10 19:35:51 +0530

    Time: 5 ms (21.44%), Space: 5.9 MB (31.73%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-     vector<int> dp(n+1,0);
-        for(int i=2;i<=n;i++)
-        {
-            for(int j=1;j<i;j++)
-            {
-                if(i%j==0)
-                {
-                    dp[i]=dp[i]|| !dp[i-j];
-                }
-            }
-        }
-        return dp[n];
+     return (n%2==0);
+        // vector<int> dp(n+1,0);
+     //    for(int i=2;i<=n;i++)
+     //    {
+     //        for(int j=1;j<i;j++)
+     //        {
+     //            if(i%j==0)
+     //            {
+     //                dp[i]=dp[i]|| !dp[i-j];
+     //            }
+     //        }
+     //    }
+     //    return dp[n];
     }
 };
