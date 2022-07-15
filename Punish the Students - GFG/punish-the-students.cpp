eb366c7d2d09c86e8b202d3a9ec6f5fffa1931bf
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-15 20:00:53 +0530

    Added solution - LeetHub

---
+++ Punish the Students - GFG/punish-the-students.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int shouldPunish (int roll[], int marks[], int n, double avg)
+    {
+        // your code here
+        int c=0;
+        for(int i=0;i<n-1;i++)
+        {
+            bool flag=false;
+            for(int j=i+1;j<n;j++)
+            {
+                if(roll[i]>roll[j])
+                {swap(roll[j],roll[j]);
+                flag=true;
+                c+=2;    
+                }
+                
+            }
+            if(!flag) break;
+        }
+        double sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=marks[i];
+        }
+        return (sum-c)/n>=avg;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		int n; cin >> n;
+		double avg; cin >> avg;
+
+		int roll[n];
+		int marks[n];
+
+		for (int i = 0; i < n; ++i)
+			cin >> roll[i];
+		for (int i = 0; i < n; ++i)
+			cin >> marks[i];
+        Solution ob;
+		cout << ob.shouldPunish (roll, marks, n, avg) << endl;
+	}
+}
+  // } Driver Code Ends
