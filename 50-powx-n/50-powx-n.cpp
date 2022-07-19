fba53bc337c91d79245c7200bef58918afa4a135
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-19 20:57:00 +0530

    Time: 7 ms (8.94%), Space: 5.9 MB (43.20%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        if(n<0)
+        {
+            x=1/x;
+            n=abs(n);
+        }
+        
+        while(n)
+        {
+            if(n%2)
+            {
+                ans*=x;
+               
+            }
+             x=x*x;
+                n/=2;
+            
+        }
+        return ans;
+    }
+};
