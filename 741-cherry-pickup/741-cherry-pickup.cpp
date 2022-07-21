c15c2f7078f36b2cd5399922b45e6ea31fd18f82
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-21 11:54:08 +0530

    Time: 1386 ms (5.06%), Space: 557.6 MB (7.47%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,int i1,int j1,int j2,vector<vector<vector<int>>> &dp)
+    int solve(vector<vector<int>>& grid,int i1,int j1,int i2,int j2,vector<vector<vector<vector<int>>>> &dp)
     {
         int m=grid[0].size(),n=grid.size();
-        int i2=j1+i1-j2;
+        // int i2=j1+i1-j2;
         
 if(j1>=m || i1>=n || j2>=m || i2>=n || grid[i1][j1]==-1 || grid[i2][j2]==-1)
             return -1e8;
-        if(dp[i1][j1][j2]!=0)
-            return dp[i1][j1][j2];
+        if(dp[i1][j1][i2][j2]!=-1)
+            return dp[i1][j1][i2][j2];
         if(i1==n-1 and j1==m-1)
         {
             return grid[i1][j1];
@@ -24,16 +24,16 @@ if(j1>=m || i1>=n || j2>=m || i2>=n || grid[i1][j1]==-1 || grid[i2][j2]==-1)
     //   DOWN    |     RIGHT
     //   RIGHT   |     DOWN
     //   RIGHT   |     RIGHT
-        x+=max({solve(grid,i1+1,j1,j2,dp),solve(grid,i1+1,j1,j2+1,dp),
-               solve(grid,i1,j1+1,j2,dp),solve(grid,i1,j1+1,j2+1,dp)
+        x+=max({solve(grid,i1+1,j1,i2+1,j2,dp),solve(grid,i1+1,j1,i2,j2+1,dp),
+               solve(grid,i1,j1+1,i2+1,j2,dp),solve(grid,i1,j1+1,i2,j2+1,dp)
                 });
-        return dp[i1][j1][j2]=x;
+        return dp[i1][j1][i2][j2]=x;
         
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
- vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(m+1,0)));
-return max(0,solve(grid,0,0,0,dp));        
+ vector<vector<vector<vector<int>>>> dp(n,vector<vector<vector<int>>>(m,vector<vector<int>>(n,vector<int>(m,-1))));
+return max(0,solve(grid,0,0,0,0,dp));        
     }
 };
