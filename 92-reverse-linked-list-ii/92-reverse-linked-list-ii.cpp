6fe022b33d70b09046bd65f7a4cba58b5e3c6973
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-21 08:59:36 +0530

    Time: 8 ms (14.28%), Space: 7.4 MB (93.11%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,24 +10,52 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(!head) return  head;
-        if(left==right) return head;
-     ListNode *dummy=new ListNode(0);
-        ListNode *pre=dummy;
-        dummy->next=head;
-        
-        for(int i=0;i<left-1;i++)
-            pre=pre->next;
-        
-        ListNode *cur=pre->next;
-        for(int i=0;i<right-left;i++)
+    ListNode* reverse(ListNode *head)
+    {
+        ListNode *cur=head;
+        ListNode *pre=NULL;
+        while(cur)
         {
-            ListNode *temp=pre->next;
-            pre->next=cur->next;
-            cur->next=cur->next->next;
-            pre->next->next=temp;
+            ListNode *nxt=cur->next;
+            cur->next=pre;
+            pre=cur;
+            cur-nxt;
         }
-        return dummy->next;
+        return pre;
+    }
+    
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head or !head->next)
+            return head;
+        if(left==right)
+            return head;
+        ListNode *f=head;
+        int c=1;
+        ListNode *fpre=NULL;
+        while(f and c<left)
+        {
+            c++;
+            fpre=f;
+            f=f->next;
+        }
+        ListNode *cur=f;
+        ListNode *pre=fpre;
+        for(int i=left;i<=right;i++)
+        {
+            ListNode *nxt=cur->next;
+            cur->next=pre;
+            pre=cur;
+            cur=nxt;
+        
+        }
+        if(left==1)
+        {
+            head=pre;
+        }
+        if(fpre)
+        fpre->next=pre;
+        f->next=cur;
+        return head;
+        
     }
 };
