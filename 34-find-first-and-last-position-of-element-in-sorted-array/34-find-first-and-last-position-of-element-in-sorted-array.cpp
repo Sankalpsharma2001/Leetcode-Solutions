4c8222d1b35ca3e7f66fac05df5ef9fa2a6a3cd2
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-25 13:22:11 +0530

    Time: 6 ms (92.71%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    int lower_bound(vector<int>& nums, int target) {
+    int l = 0, r = nums.size()-1;
+    while (l <= r) {
+        int mid = (r-l)/2+l;
+        if (nums[mid] < target)
+            l = mid+1;
+        else
+            r = mid-1;
+    }
+    return l;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
-        int x=-1,y=-1;
-        if(nums.size()==0)
-            return {-1,-1};
-        int idx1=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int idx2=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-       if(!binary_search(nums.begin(),nums.end(),target))    return {-1,-1};
-        if(nums[idx1]!=target)
-            return {-1,-1};
-          idx2--;
-        return {idx1,idx2};
-        
+         int idx1 = lower_bound(nums, target);
+    int idx2 = lower_bound(nums, target+1)-1;
+    if (idx1 < nums.size() && nums[idx1] == target)
+        return {idx1, idx2};
+    else
+        return {-1, -1};
     }
 };
