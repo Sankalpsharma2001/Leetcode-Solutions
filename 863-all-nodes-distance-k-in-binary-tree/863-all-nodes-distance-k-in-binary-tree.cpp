55ae519711117f79208c4f4372544e5ee128bc4f
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-31 12:18:00 +0530

    Time: 11 ms (51.25%), Space: 13 MB (44.56%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,109 +9,67 @@
  */
 class Solution {
 public:
-    map<TreeNode*,TreeNode*> parent;
-    void find(TreeNode* root,TreeNode* target,TreeNode *par)
-    {
-        if(!root) return;
-      
-       
-       parent[root]=par;
-        find(root->left,target,root);
-        
-        find(root->right,target,root);
-    }
-    void findatk(TreeNode* root,int k,vector<int> &ans,vector<int> &vis)
-    {
-        if(!root) return;
-        
-        if(k==0 and !vis[root->val])
-        { 
-   vis[root->val]=1;
-            ans.push_back(root->val);
-            return;
-        }
-        
-        if(root->left and !vis[root->left->val])
-        findatk(root->left,k-1,ans,vis);
-        
-        if(root->right and !vis[root->right->val])
-        findatk(root->right,k-1,ans,vis);
-    }
-    void findinpar(TreeNode* root,int k,vector<int> &ans,vector<int> &vis)
-    {
-        if(!root) return;
-    
-        if(k==0 and !vis[root->val])
-        {
-            
-   vis[root->val]=1;
-            ans.push_back(root->val);
-        }
-        vis[root->val]=1;
-        if(root->left and !vis[root->left->val])
-            findinpar(root->left,k-1,ans,vis);
-        if(root->right and !vis[root->right->val])
-            findinpar(root->right,k-1,ans,vis);
-        
-       if(parent[root] and !vis[parent[root]->val] )
-          findinpar(parent[root],k-1,ans,vis);
-    }
-    void findall(TreeNode* root,int k,vector<int> &ans,vector<int> &vis)
-    {
-        if(!root) return;
-        vis[root->val]=1;
-        
-        if(k==0)
-        {          
-          ans.push_back(root->val);
-        }
-    
-        if(root->left and !vis[root->left->val])
-            findinpar(root->left,k-1,ans,vis);
-        if(root->right and !vis[root->right->val])
-            findinpar(root->right,k-1,ans,vis);
-        
-       if(parent[root] and !vis[parent[root]->val] )
-          findinpar(parent[root],k-1,ans,vis);
-    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        parent.clear();
-        vector<int> ans;
-        if(!root) return ans;
-        if(k==0)
-        {ans.push_back(target->val);
-            return ans;
-        }
-        vector<int> vis(501,0);
-        // find(root,target,NULL);
-       // bfs we can use also
-         queue<TreeNode*> q;
-        
+       map<TreeNode*,TreeNode*> m;
+        queue<TreeNode*> q;
+        TreeNode *f=NULL;
         q.push(root);
-        parent[root] = NULL;
-        
+        m[root]=NULL;
         while(!q.empty())
         {
-            TreeNode* left = q.front()->left;
-            TreeNode* right = q.front()->right;
-            TreeNode* parent1 = q.front();
-            
-            if(left)
+            auto t=q.front();
+            q.pop();
+            if(t->val==target->val)
+                f=target;
+            if(t->left)
             {
-                q.push(left);
-                parent[left] = parent1;
+                q.push(t->left);
+                m[t->left]=t;
             }
-            if(right)
+            if(t->right)
             {
-                q.push(right);
-                parent[right] = parent1;
+                q.push(t->right);
+                m[t->right]=t;
             }
-            
-            q.pop();    
         }
-        findall(target,k,ans,vis);
-        // findatk(target,k,ans,vis);
-        // findinpar(target,k,ans,vis);
+       queue<TreeNode*> q1;
+        
+        map<TreeNode*,int> vis;
+        q1.push(target);
+        vis[target]=1;
+        int dis=0;
+        while(!q1.empty())
+        {
+            
+            int sz=q1.size();
+                            if(dis==k) break;
+
+            while(sz--)
+            {
+                                if(dis==k) break;
+               auto x=q1.front();
+                vis[x]=1;
+                q1.pop();
+                if(x->left and !vis[x->left])
+                {
+                 q1.push(x->left);   
+                }
+                if(x->right and !vis[x->right])
+                {
+                    q1.push(x->right);
+                }
+                if(m[x] and !vis[m[x]])
+                    q1.push(m[x]);
+            }
+            dis++;
+        }
+            vector<int> ans;
+        while(!q1.empty())
+        {
+            ans.push_back(q1.front()->val);
+            q1.pop();
+        }
         return ans;
+        
     }
 };
