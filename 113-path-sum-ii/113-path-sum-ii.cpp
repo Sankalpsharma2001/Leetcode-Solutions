052f457505b9a9149ec07c9a72b8fabee4502e6b
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-31 16:39:53 +0530

    Time: 38 ms (17.82%), Space: 42.6 MB (6.33%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,25 +11,38 @@
  */
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void solve(TreeNode *root,int targetSum,int s,vector<int> v)
-    {
-        if(!root) return;
-        v.push_back(root->val);
-        if(!root->left and !root->right and s+root->val==targetSum)
-        {
-            ans.push_back(v);
-            return;
-        }
-        solve(root->left,targetSum,s+root->val,v);
-        solve(root->right,targetSum,s+root->val,v);
-        v.pop_back();
-    } 
-        
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-      ans.clear();
-        vector<int> v;
-        solve(root,targetSum,0,v);
+        queue< pair< TreeNode*,pair<int,vector<int> > > > q;
+    vector<vector<int>> ans;
+    if(!root)
+        return ans;
+        q.push({root,{root->val,{root->val}}});
+        while(!q.empty())
+        {
+            auto f=q.front();
+            q.pop();
+            TreeNode *t=f.first;
+            int c=f.second.first;
+            vector<int> v=f.second.second;
+            if(!t->left and !t->right and c==targetSum)
+            {
+                ans.push_back(v);
+            }
+            if(t->left)
+            {
+                vector<int> l=v;
+                l.push_back(t->left->val);
+                q.push({t->left,{c+t->left->val,l}});
+                
+            }
+             if(t->right)
+            {
+                vector<int> r=v;
+                r.push_back(t->right->val);
+                q.push({t->right,{c+t->right->val,r}});
+                
+            }
+        }
         return ans;
     }
 };
