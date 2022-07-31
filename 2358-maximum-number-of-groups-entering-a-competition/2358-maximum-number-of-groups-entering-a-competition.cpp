7ef13215ee7b23e11dc0313ca0e96c3885f09374
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-07-31 10:54:32 +0530

    Time: 197 ms (100.00%), Space: 56.5 MB (100.00%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
         sort(grades.begin(),grades.end());
-       int n=grades.size();
-        int i=1,sum=0;
-        while(sum+i<=n)
+      int low=0,high=grades.size();
+        int n=grades.size(),ans=1;
+        while(low<=high)
         {
-            sum+=i;
-            i++;
+            int mid=low+(high-low)/2;
+            if(1ll*mid*(mid+1)/2<=n)
+            {
+                ans=mid;
+                low=mid+1;
+            }
+            else
+                high=mid-1;
         }
-        return i-1;
+        return ans;
     }
 };
