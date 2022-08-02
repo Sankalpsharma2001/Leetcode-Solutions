fe089beabdbb0c2745caf432d974cf2bfe7d0208
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-08-02 08:52:45 +0530

    Time: 31 ms (93.99%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -3,24 +3,27 @@ public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n=matrix.size();
         int m=matrix[0].size();
-        // NLOGK if we use sorting then NLogN
-        priority_queue<int,vector<int>> pq;
-       for(int i=0;i<n;i++)
-       {
-           for(int j=0;j<m;j++)
-           {
-               if(pq.size()<k)
-                   pq.push(matrix[i][j]);
-               else
-               {
-                   if(pq.top()>matrix[i][j])
-                   {
-                       pq.pop();
-                       pq.push(matrix[i][j]);
-                   }
-               }
-           }
-       }
-        return pq.top();
+       int low=matrix[0][0],high=matrix[n-1][m-1],ans=matrix[0][0];
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+         int col=n-1,cnt=0;
+            for(int i=0;i<n;i++)
+            {
+                while(col>=0 and matrix[i][col]>mid) col--;
+                cnt+=col+1;
+                
+            }
+            if(cnt<k)
+            {
+                ans=mid+1;
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return ans;
     }
 };
