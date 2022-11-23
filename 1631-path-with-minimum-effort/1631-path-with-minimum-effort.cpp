b47418ccb188626649ed63f6c7e098ee5eba138d
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-11-23 18:15:22 +0530

    Time: 265 ms (79.10%), Space: 20 MB (66.73%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,55 +1,42 @@
+#define pipi pair<int,pair<int,int>> 
 class Solution {
 public:
-    bool isValid(vector<vector<int>> &heights,int mid)
+    bool isValid(int x,int y,int n,int m)
     {
-        queue<pair<int,int>> q;
-        int dx[4]={1,-1,0,0};
-        int dy[4]={0,0,-1,1};
-        q.push({0,0});
-        int n=heights.size(),m=heights[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        while(!q.empty())
-        {
-            auto node=q.front();
-            q.pop();
-            int x=node.first;
-            int y=node.second;
-            if(x==n-1 && y==m-1)
-                return true;
-            for(int ind=0;ind<4;ind++)
-            {
-                int xx=dx[ind]+x;
-                int yy=dy[ind]+y;
-                if(xx<0 || xx>=n || yy<0 || yy>=m) continue;
-                if(vis[xx][yy]) continue;
-                if(abs(heights[xx][yy]-heights[x][y])<=mid) 
-                {
-                    vis[xx][yy]=1;
-                    q.push({xx,yy});
-                }
-                
-            }    
-                
-        }
-        return false;
+        return x>=0 && y>=0 && x<n && y<m;
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        
-       int l=0,r=1e9,ans=1e9;
-        while(l<=r)
+        int n=heights.size();
+        int m=heights[0].size();
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,-1,1};
+        priority_queue<pipi,vector<pipi>,greater<pipi>> pq;
+        vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
+        dis[0][0]=0;
+        pq.push({0,{0,0}});
+        while(!pq.empty())
         {
-            int mid=l+(r-l)/2;
-            if(isValid(heights,mid))
-            {
-                ans=mid;
-                r=mid-1;
-            }
+            auto top=pq.top();
+            pq.pop();
+            int dif=top.first;
+            int x=top.second.first;
+            int y=top.second.second;
+            if(dis[x][y]<dif) continue;
+            if(x==n-1 && y==m-1)
+                return dif;
             
-            else
+            for(int ind=0;ind<4;ind++)
             {
-                l=mid+1;
+                int xx=x+dx[ind];
+                int yy=y+dy[ind];
+                if(!isValid(xx,yy,n,m)) continue;
+                int newdif=max(abs(heights[xx][yy]-heights[x][y]),dif);
+                if(dis[xx][yy]<=newdif) continue;
+                    dis[xx][yy]=newdif;
+                    pq.push({newdif,{xx,yy}});
+                
             }
         }
-        return ans;
+        return 0;
     }
 };
