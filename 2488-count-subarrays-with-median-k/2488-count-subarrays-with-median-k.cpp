75f56c14c7449726dd5a142ef34d692541d4c769
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2022-11-27 20:05:42 +0530

    Time: 422 ms (37.50%), Space: 53.5 MB (50.00%) - LeetHub

--- 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.cpp
+++ 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
     int countSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        int idx=0;
-        while(nums[idx]!=k)idx++;
+        // int n=nums.size();
+        // int idx=0;
+        // while(nums[idx]!=k)idx++;
+        // map<int,int> m;
+        // m[0]=1;
+        // int sum=0;
+        // for(int i=idx+1;i<n;i++)
+        // {
+        //         sum+=(nums[i]<k)?-1:1;
+        //     m[sum]++;
+        // }
+        // int ans=m[0]+m[1];
+        // sum=0;
+        // for(int i=idx-1;i>=0;i--)
+        // {
+        //     sum+=(nums[i]<k)?-1:1;
+        //     ans+=(m[-sum]+m[1-sum]);
+        // }
+        // return ans;
         map<int,int> m;
-        m[0]=1;
-        int sum=0;
-        for(int i=idx+1;i<n;i++)
+        int n=nums.size();
+        int sum=0,ans=0;
+        m[0]++;
+        bool flag=false;
+        for(int i=0;i<n;i++)
         {
-                sum+=(nums[i]<k)?-1:1;
-            m[sum]++;
-        }
-        int ans=m[0]+m[1];
-        sum=0;
-        for(int i=idx-1;i>=0;i--)
-        {
-            sum+=(nums[i]<k)?-1:1;
-            ans+=(m[-sum]+m[1-sum]);
+            if(nums[i]<k)
+                sum--;
+            if(nums[i]>k)
+                sum++;
+            if(nums[i]==k)
+                flag=true;
+            if(flag)
+            {
+                      // even  odd
+                ans+=m[sum]+m[sum-1];
+            }
+            else
+            {
+                m[sum]++;
+            }
         }
         return ans;
     }
