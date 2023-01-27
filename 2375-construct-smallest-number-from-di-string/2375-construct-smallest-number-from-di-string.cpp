0443b21877a4410bf19937409f54b3f914038e6b
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-01-27 19:52:07 +0530

    Time: 109 ms (11.98%), Space: 5.9 MB (75.66%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,18 +1,85 @@
 class Solution {
 public:
+    string ans="987654321";
     string smallestNumber(string pattern) {
-        int n=pattern.size();
-        string ans="";
-        int j=0;
-        for(int i=0;i<=pattern.size();i++)
+        // BACKTRACKING
+        string s="";
+        vector<int> vis(10,0);
+        solve(pattern,vis,-1,s);
+        return ans;
+        
+        
+         // GREEDY
+        // int n=pattern.size();
+        // string ans="";
+        // int j=0;
+        // for(int i=0;i<=pattern.size();i++)
+        // {
+        //   ans+=('1'+i);
+        //     if(i==pattern.size() || pattern[i]=='I')
+        //     {
+        //         reverse(ans.begin()+j,ans.end());
+        //         j=i+1;
+        //     }
+        // }
+        // return ans;
+    }
+    void solve(string &pattern,vector<int> &vis,int i,string s)
+    {
+        if(i==pattern.size())
         {
-          ans+=('1'+i);
-            if(i==pattern.size() || pattern[i]=='I')
+            if(ans>s)
             {
-                reverse(ans.begin()+j,ans.end());
-                j=i+1;
+                ans=s;
+            }
+            return;
+        }
+        if(i==-1)
+        {
+            for(int j=1;j<=9;j++)
+            {
+                if(vis[j]==0)
+                {
+                    vis[j]=1;
+                    s+=j+'0';
+                    solve(pattern,vis,i+1,s);
+                    s.pop_back();
+                    vis[j]=0;
+                }
             }
         }
-        return ans;
+        else
+        {
+            if(pattern[i]=='I')
+            {
+                int temp=s[s.size()-1]-'0';
+                for(int j=temp+1;j<=9;j++)
+                {
+                    if(vis[j]==0)
+                    {
+                         vis[j]=1;
+                    s+=j+'0';
+                    solve(pattern,vis,i+1,s);
+                    s.pop_back();
+                    vis[j]=0;
+                    }
+                }
+            }
+            else
+            {
+                int temp=s[s.size()-1]-'0';
+                 for(int j=temp-1;j>=1;j--)
+                {
+                    if(vis[j]==0)
+                    {
+                         vis[j]=1;
+                    s+=j+'0';
+                    solve(pattern,vis,i+1,s);
+                    s.pop_back();
+                    vis[j]=0;
+                    }
+                }
+            }
+        }
     }
 };
