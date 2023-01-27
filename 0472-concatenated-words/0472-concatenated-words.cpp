5e412990b9ba3225c75abce8cae24f7d84687bc1
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-01-27 12:29:43 +0530

    Time: 263 ms (66.48%), Space: 56.2 MB (46.74%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
+    int noOfConcatedStrings(string word, unordered_map<string, int> &dp, unordered_set<string> &uniqueWords) {
+        if(word == "") return 0;
+        
+        // If we've already calculated the 
+        //concatenated string count for current word, 
+        //return the count
+        if(dp.count(word)) return dp[word];
+        
+        int sz = word.length();
+        string current = "";       // to form possible combination
+        int mxCount = INT_MIN;     // count of smaller concatenated strings
+        
+        for(int i = 0; i < sz; i++) 
+        {
+            current += word[i];          // form the possible combination
+            if(uniqueWords.count(current)) // does combination belongs to set of given words??
+            {
+                // increase count by 1 and find count for remaining word      
+                mxCount = max(mxCount, 1 + noOfConcatedStrings(word.substr(i + 1), dp, uniqueWords)); 
+                
+            }
+        }
+        
+        return dp[word] = mxCount;       // store count for current word
+    }
+ 
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string> s;
+        unordered_set<string> uniqueWords;
         for(string &it:words) {
-            s.insert(it);
+            uniqueWords.insert(it);
         }
         vector<string> ans;
+        unordered_map<string,int> dp;
         for(auto &w:words)
         {
             int n=w.size();
-            vector<int> dp(n+1,0);
-            dp[0]=1;
-            for(int i=0;i<n;i++)
-            {
-                if (!dp[i]) continue;
-                for(int j=i+1;j<=n;j++)
-                {
-                    if(j-i<n && s.count(w.substr(i,j-i)))
-                    {
-                        dp[j]=1;
-                    }
-                }
-            }
-            if(dp[n])
+            if(noOfConcatedStrings(w,dp,uniqueWords)>=2)
             {
                 ans.push_back(w);
             }
