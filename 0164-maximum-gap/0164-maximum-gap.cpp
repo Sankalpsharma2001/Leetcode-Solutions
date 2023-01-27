1194f791e25ea73b0b87209dfaa167b1b95781e6
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-01-27 11:28:42 +0530

    Time: 174 ms (97.03%), Space: 89.1 MB (37.14%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -3,12 +3,33 @@ public:
     int maximumGap(vector<int>& nums) {
         int n=nums.size();
         if(n<2) return 0;
-        sort(nums.begin(),nums.end());
-        int ans=0;
-        for(int i=1;i<n;i++)
+        int mn=INT_MAX,mx=INT_MIN;
+        for(int n:nums)
         {
-          ans=max(ans,nums[i]-nums[i-1]);  
+            mn=min(n,mn);
+            mx=max(n,mx);
         }
-        return ans;
+        int interval=(int)ceil((double)(mx-mn)/(n-1));
+        vector<int> bucketMin(n-1,INT_MAX);
+        vector<int> bucketMax(n-1,-1);
+        for(int i=0;i<n;i++)
+        {
+          if(nums[i]==mn || nums[i]==mx)  continue;  
+            int index=(nums[i]-mn)/interval;
+            bucketMin[index]=min(bucketMin[index],nums[i]);
+            bucketMax[index]=max(bucketMax[index],nums[i]);
+            
+        }
+        int prev=mn;
+        int gap=0;
+         for(int i=0;i<bucketMin.size();i++)
+         {
+             if(bucketMax[i]==-1) continue;
+             gap=max(gap,bucketMin[i]-prev);
+             prev=bucketMax[i];
+         }
+        gap=max(gap,mx-prev);
+        return gap;
+        
     }
 };
