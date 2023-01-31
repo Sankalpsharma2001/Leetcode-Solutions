66c4695e0980768011006d685b00c26833e45fa2
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-01-31 19:14:06 +0530

    Time: 518 ms (22.07%), Space: 133.5 MB (20.45%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    int solve(int curIndex,int prevIndex,vector<pair<int,int>> &p,vector<vector<int>> &dp)
+    {
+        if(curIndex>=p.size()) return 0;
+        if(dp[curIndex][prevIndex+1]!=-1) return dp[curIndex][prevIndex+1];
+        int notPick=0;
+        int pick=0;
+        if(prevIndex==-1 || p[curIndex].second>=p[prevIndex].second)
+        {
+            pick=p[curIndex].second+solve(curIndex+1,curIndex,p,dp);
+        }
+        notPick=solve(curIndex+1,prevIndex,p,dp);
+        return dp[curIndex][prevIndex+1]=max(pick,notPick);
+    }
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n=scores.size();
         vector<pair<int,int>> p;
@@ -8,20 +21,25 @@ public:
          p.push_back({ages[i],scores[i]});   
         }
         sort(p.begin(),p.end());
-        int ans=0;
-        vector<int> dp(n+1,0);
-        for(int i=0;i<n;i++)
-        {
-            dp[i]=p[i].second;
-         for(int j=0;j<i;j++)
-         {
-             if(p[i].second>=p[j].second)
-             {
-                 dp[i]=max(dp[i],dp[j]+p[i].second);
-             }
-         }
-          ans=max(ans,dp[i]);  
-        }
-        return ans;
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return solve(0,-1,p,dp);
+        
+        
+        // sort(p.begin(),p.end());
+        // int ans=0;
+        // vector<int> dp(n+1,0);
+        // for(int i=0;i<n;i++)
+        // {
+        //     dp[i]=p[i].second;
+        //  for(int j=0;j<i;j++)
+        //  {
+        //      if(p[i].second>=p[j].second)
+        //      {
+        //          dp[i]=max(dp[i],dp[j]+p[i].second);
+        //      }
+        //  }
+        //   ans=max(ans,dp[i]);  
+        // }
+        // return ans;
     }
 };
