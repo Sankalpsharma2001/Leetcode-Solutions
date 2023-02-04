188a424abe18b30458b3fc969bdf44c45409eb90
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-02-04 15:32:59 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -8,36 +8,32 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-int dp[1000005];
-int solve(int idx,int *arr)
-{
-    if(idx==0) return arr[0];
-    if(idx<0) return 0;
-    if(dp[idx]!=-1) return dp[idx];
-    int pick=arr[idx]+solve(idx-2,arr);
-    int notPick=solve(idx-1,arr);
-    return dp[idx]=max(pick,notPick);
-}
+// int dp[1000005];
+// int solve(int idx,int *arr)
+// {
+//     if(idx==0) return arr[0];
+//     if(idx<0) return 0;
+//     if(dp[idx]!=-1) return dp[idx];
+//     int pick=arr[idx]+solve(idx-2,arr);
+//     int notPick=solve(idx-1,arr);
+//     return dp[idx]=max(pick,notPick);
+// }
 	// calculate the maximum sum with out adjacent
 	int findMaxSum(int *arr, int n) {
 	    // code here
-	    memset(dp,-1,sizeof(dp));
-	   return solve(n-1,arr);
+	   // memset(dp,-1,sizeof(dp));
+	   //return solve(n-1,arr);
 	    
-	   // vector<int> dp(n+1);
-	   // if(n==1)
-	   // return arr[0];
-	   // if(n==2)
-	   // return max(arr[0],arr[1]);
-	   // dp[0]=arr[0];
-	   // dp[1]=max(arr[0],arr[1]);
-	   // for(int i=2;i<n;i++)
-	   // {
-	   //     int notTake=dp[i-1];
-	   //     int take=dp[i-2]+arr[i];
-	   //     dp[i]=max(notTake,take);
-	   // }
-	   // return dp[n-1];
+	    vector<int> dp(n+1);
+	    dp[0]=arr[0];
+	    for(int i=1;i<n;i++)
+	    {
+	        int notTake=dp[i-1];
+	        int take=arr[i];
+	        if(i>1) take+=dp[i-2];
+	        dp[i]=max(notTake,take);
+	    }
+	    return dp[n-1];
 	}
 };
 
