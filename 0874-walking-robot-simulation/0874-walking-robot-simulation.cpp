25fc3cb758c14c5b6aaa5ba311627b7d6c394cff
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-02-14 15:09:31 +0530

    Time: 215 ms (26.34%), Space: 34.6 MB (67.86%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,116 +1,24 @@
 class Solution {
 public:
-    double distance(int x,int y)
-    {
-        return (x*x+y*y);
-    }
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        set<pair<int,int>> s;
-        for(auto &it:obstacles)
-        {
-            s.insert({it[0],it[1]});
-        }
-        int x=0,y=0;
-        int ans=INT_MIN;
-        int north=0,east=0,south=0,west=0;
-        north=1;
-        for(int i=0;i<commands.size();i++)
-        {
-            if(commands[i]==-2)
-            {
-                 
-            if(north)
-            {
-             west=1;
-                north=0;
-            }
-           else if(south)
-            {
-                east=1;
-                south=0;
-            }
-           else if(west)
-            {
-                south=1;
-                west=0;
-            }
-           else if(east)
-            {
-                north=1;
-                east=0;
-            }
-                
-            }
-           else if(commands[i]==-1)
-            {
-                 if(north)
-            {
-             east=1;
-                north=0;
-            }
-           else if(south)
-            {
-                west=1;
-                south=0;
-            }
-           else if(west)
-            {
-                north=1;
-                west=0;
-            }
-           else if(east)
-            {
-                south=1;
-                east=0;
-            }
-                  
-            }
-            else 
-            {
-                int c=commands[i];
-                while(c--)
-                {
-                      if(north)
-            {
-                          if(s.find({x,y+1})!=s.end())
-                    {
-                        break;
-                    }
-              y++;
-            }
-            if(south)
-            {
-                if(s.find({x,y-1})!=s.end())
-                    {
-                        break;
-                    }
-                y--;
-            }
-            if(west)
-            {
-                if(s.find({x-1,y})!=s.end())
-                    {
-                        break;
-                    }
-                x--;
-            }
-            if(east)
-            {
-                if(s.find({x+1,y})!=s.end())
-                    {
-                        break;
-                    }
-               x++;
-            }
-                   
-                    
-               
+         unordered_set<string> obs;
+        for(int i=0;i<obstacles.size();i++) obs.insert(to_string(obstacles[i][0])+"#"+to_string(obstacles[i][1]));
+        int res=0, dir=0, x=0, y=0;
+        vector<vector<int>> ds={{0,1}, {1,0}, {0,-1}, {-1,0}};
+        for(int i=0;i<commands.size();i++) {
+            if(commands[i]==-2) dir--;
+            else if(commands[i]==-1) dir++;
+            else {
+                for(int j=0;j<commands[i];j++) {
+                    string pos=to_string(x+ds[dir][0])+"#"+to_string(y+ds[dir][1]);
+                    if(obs.find(pos)!=obs.end()) break;
+                    x+=ds[dir][0], y+=ds[dir][1];
                 }
+                res=max(res, x*x+y*y);
             }
-             ans=max(ans,(int)distance(x,y));
-           // cout<<x<<" "<<y<<" "<<ans<<endl; 
-           
+            if(dir==-1) dir=3;
+            if(dir==4) dir=0;
         }
-        return ans;
+        return res;
     }
 };
