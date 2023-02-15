6c95da4e1496a243e93e4f446eae431f08ec4927
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-02-15 23:25:38 +0530

    Time: 52 ms (68.97%), Space: 32.7 MB (52.96%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,59 +1,44 @@
 class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        vector<vector<int>> adj(n);
-        int edge=0;
-        for(int i=0;i<n;i++)
-        {
-            if(leftChild[i]!=-1)
-            {
-                adj[i].push_back(leftChild[i]);
-                edge++;
+        vector<int> indegree(n,0);
+        for (int i=0;i<n;i++) {
+            if(leftChild[i]!=-1 && ++indegree[leftChild[i]]>1){
+                return false;
             }
-            if(rightChild[i]!=-1)
-            {
-                adj[i].push_back(rightChild[i]);
-                edge++;
+            if(rightChild[i]!=-1 && ++indegree[rightChild[i]]>1){
+                return false;
             }
+        }
+        int root=-1;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                if(root!=-1){  
+                    return false;
+                }
+                root=i;
+            }
+        }
+          if(root==-1){ 
+            return false;
         }
         
-        vector<int> indegree(n,0);
-        for(int i=0;i<adj.size();i++)
-        {
-            for(int j=0;j<adj[i].size();j++)
-            {
-                indegree[adj[i][j]]++;
-            }
-        }
-        queue<int> q;
-        int totalNodes=0;
-        for(int i=0;i<n;i++)
-        {
-            if(indegree[i]>1)
+         vector<bool> vis(n,false);
+        traverseTree(leftChild,rightChild,root,vis);
+         for(auto e: vis){
+            if(e==false){ 
                 return false;
-            if(indegree[i]==0)
-            {
-                q.push(i);
-                totalNodes++;
             }
         }
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(auto &it:adj[node])
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
-                {
-                    q.push(it);
-                    totalNodes++;
-                }
-            }
-            
+        return true; 
+    }
+     void traverseTree(vector<int> &left, vector<int> &right, int root, vector<bool> &vis) {
+        if(root==-1 || vis[root]==true){
+            return;
         }
-        if(totalNodes!=n || edge!=n-1)
-            return false;
-        return true;
+        vis[root]=true;
+        traverseTree(left,right,left[root],vis);
+        traverseTree(left,right,right[root],vis);
+        return;
     }
 };
