baa25dcff0041e440f2d63a200ae474d93347048
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-02-25 11:48:09 +0530

    Time: 3 ms (72.61%), Space: 8.6 MB (52.77%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -4,14 +4,26 @@ public:
         sort(citations.begin(),citations.end());
        
         int len=citations.size();
-        for(int i=0;i<citations.size();i++)
-        {
-            if(len-i<=citations[i])
-            {
-                return len-i;
-            }
+//         for(int i=0;i<citations.size();i++)
+//         {
+//             if(len-i<=citations[i])
+//             {
+//                 return len-i;
+//             }
            
+//         }
+        int low=0,high=len-1,ans=0;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(len-mid<=citations[mid])
+            {
+                ans=len-mid;
+                high=mid-1;
+            }
+            else
+                low=mid+1;
         }
-        return 0;
+        return ans;
     }
 };
