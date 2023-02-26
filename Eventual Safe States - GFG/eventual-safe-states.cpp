bba7f26f5aa21194a3b49f936ad27eefd7bf0d6d
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-02-26 16:03:12 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,40 +10,51 @@ using namespace std;
 
 class Solution {
   public:
+    bool dfs(int node,vector<int> adj[],vector<int> &vis,vector<int> &pathVis,vector<int> &check)
+    {
+        vis[node]=1;
+        pathVis[node]=1;
+        check[node]=0;
+        
+        for(auto &it:adj[node])
+        {
+            if(!vis[it])
+            {
+            if(dfs(it,adj,vis,pathVis,check)==true)
+            {
+                check[node]=0;
+                return true;
+            }
+            }
+            else if(pathVis[it])
+            {
+                check[node]=0;
+                return true;
+            }
+        }
+        check[node]=1;
+        pathVis[node]=0;
+        return false;
+    }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
-        vector<int> adjRev[V];
-        vector<int> indegree(V,0);
-        for(int i=0;i<V;i++)
-        {
-            for(auto &it:adj[i])
-            {
-                adjRev[it].push_back(i);
-                indegree[i]++;
-            }
-        }
-        queue<int> q;
-        for(int i=0;i<V;i++)
-        {
-            if(indegree[i]==0)
-            q.push(i);
-            
-        }
-        vector<int> topo;
-        while(!q.empty())
-        {
-            auto f=q.front();
-            q.pop();
-            topo.push_back(f);
-            for(auto &it:adjRev[f])
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
-                q.push(it);
-            }
-        }
-        sort(topo.begin(),topo.end());
-        return topo;
+       vector<int> vis(V,0);
+       vector<int> pathVis(V,0);
+       vector<int> check(V,0);
+       for(int i=0;i<V;i++)
+       {
+           if(!vis[i])
+           {
+               dfs(i,adj,vis,pathVis,check);
+           }
+       }
+       vector<int> ans;
+       for(int i=0;i<V;i++)
+       {
+           if(check[i]==1)
+           ans.push_back(i);
+       }
+       return ans;
     }
 };
 
