97e0f30f351ccfdd4622ef1a1256bb7095f5d463
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-03-18 16:04:23 +0530

    Time: 7 ms (66.24%), Space: 8.5 MB (99.42%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,24 +20,47 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
        int n=triangle.size();
         // vector<vector<int>> dp(n,vector<int>(n,-1));
+          // return solve(0,0,triangle,dp);
+      
         
-        vector<vector<int>> dp(n,vector<int>(n,0));
+//         vector<vector<int>> dp(n,vector<int>(n,0));
+//         for(int i=0;i<n;i++)
+//         {
+//             dp[n-1][i]=triangle[n-1][i];
+//         }
+//         for(int i=n-2;i>=0;i--)
+//         {
+//             for(int j=i;j>=0;j--)
+//             {
+//                 int d=triangle[i][j]+dp[i+1][j];
+//                 int dr=triangle[i][j]+dp[i+1][j+1];
+//                 dp[i][j]=min(d,dr);
+//             }
+//         }
+        
+//         return dp[0][0];
+        
+        
+        
+        
+      vector<int> v(n,0),cur(n,0);
         for(int i=0;i<n;i++)
         {
-            dp[n-1][i]=triangle[n-1][i];
+            v[i]=triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--)
         {
             for(int j=i;j>=0;j--)
             {
-                int d=triangle[i][j]+dp[i+1][j];
-                int dr=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(d,dr);
+                int d=triangle[i][j]+v[j];
+                int dr=triangle[i][j]+v[j+1];
+                cur[j]=min(d,dr);
             }
+            v=cur;
         }
         
-        return dp[0][0];
+        return v[0];
         
-        // return solve(0,0,triangle,dp);
+        
     }
 };
