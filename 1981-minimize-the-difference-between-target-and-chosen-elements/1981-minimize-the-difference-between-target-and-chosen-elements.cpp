395ccf30bbbabeb3bfff349547633ffe20e7115b
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-04-07 17:07:50 +0530

    Time: 2092 ms (33.78%), Space: 171.2 MB (6.76%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,24 +1,27 @@
-class Solution {
-public:
-    int n,m;
-    int minDif(int row,int sum,vector<vector<int>>& mat, int target,  vector<vector<int>> &dp)
+class Solution
+{
+    public:
+        int n, m;
+    int minDif(int row, int sum, vector<vector < int>> &mat, int target, vector< vector< int>> &dp)
     {
-      if(row>=n)
-      {
-          return abs(target-sum);
-      }
-        if(dp[row][sum]!=-1)
-            return dp[row][sum];
-        int temp=INT_MAX;
-        for(int col=0;col<m;col++)
+        if (row >= n)
         {
-            temp=min(temp,minDif(row+1,sum+mat[row][col],mat,target,dp));
+            return abs(target - sum);
         }
-        return dp[row][sum]=temp;
+        if (dp[row][sum] != -1)
+            return dp[row][sum];
+        int temp = INT_MAX;
+        for (int col = 0; col < m; col++)
+        {
+            temp = min(temp, minDif(row + 1, sum + mat[row][col], mat, target, dp));
+            if (temp == 0) break;
+        }
+        return dp[row][sum] = temp;
     }
-    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-         n=mat.size(),m=mat[0].size();
-        vector<vector<int>> dp(71,vector<int>(5000,-1));
-        return minDif(0,0,mat,target,dp);
+    int minimizeTheDifference(vector<vector < int>> &mat, int target)
+    {
+        n = mat.size(), m = mat[0].size();
+        vector<vector < int>> dp(71, vector<int> (5000, -1));
+        return minDif(0, 0, mat, target, dp);
     }
 };
