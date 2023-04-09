8a8743317f9b5a54e9fa66f87085767e08287b49
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-04-09 10:38:01 +0530

    Time: 1543 ms (14.29%), Space: 236.3 MB (57.14%) - LeetHub

--- 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.cpp
+++ 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int minimumVisitedCells(vector<vector<int>>& grid) {
-          priority_queue<pair<long long,pair< int, int>>, vector<pair<long long,pair< int, int>>>, greater<pair<long long,pair< int, int>>>> pq;
+            priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
         int n=grid.size(),m=grid[0].size();
         vector<vector<bool>> visited(n,vector<bool>(m,false));
-        pq.push({1,{0,0}});
-      
+        pq.push({1,0,0});
+        visited[0][0]=1;
         while(!pq.empty()){
             auto it=pq.top(); pq.pop();
-            int a=it.first,b=it.second.first,c=it.second.second;
+            int a=it[0],b=it[1],c=it[2];
             if(b==n-1 && c==m-1) return a;
-            if(grid[b][c]==0) continue;
+           
             for(int i=c+1;i<m && i<=grid[b][c]+c;i++){
                 if(!visited[b][i]){
-                     visited[b][i]=1;
-                    if(b==n-1 && i==m-1)
+                    visited[b][i]=1;
+                      if(b==n-1 && i==m-1)
                         return a+1;
-                    pq.push({a+1,{b,i}});
+                    pq.push({a+1,b,i});
                 }
             }
             for(int i=b+1;i<n && i<=grid[b][c]+b;i++){
                 if(!visited[i][c]){
-                     visited[i][c]=1;
-                     if(i==n-1 && c==m-1)
+                    visited[i][c]=1;
+                      if(i==n-1 && c==m-1)
                         return a+1;
-                    pq.push({a+1,{i,c}});
+                    pq.push({a+1,i,c});
                 }
             }
         }
