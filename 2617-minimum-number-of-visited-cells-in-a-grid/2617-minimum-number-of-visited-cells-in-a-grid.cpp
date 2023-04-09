99b223e04594bda7a3de3321af09950b9a8b095d
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-04-09 10:36:16 +0530

    Time: 659 ms (14.29%), Space: 146.5 MB (57.14%) - LeetHub

--- 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.cpp
+++ 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int minimumVisitedCells(vector<vector<int>>& grid) {
-         int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<long long>> distance(m,(vector<long long>(n,INT_MAX)));
-        distance[0][0]=1;
-        priority_queue<pair<long long,pair< int, int>>, vector<pair<long long,pair< int, int>>>, greater<pair<long long,pair< int, int>>>> pq;
-        pq.push({1,{0, 0}} );
-        while (!pq.empty()) {
-            long long d = pq.top().first;
-            int i=pq.top().second.first;
-            int j=pq.top().second.second;
-            pq.pop();
-            if (i == m-1 && j == n-1) {
-                return (int)d;
-            }
-            if(grid[i][j]==0) continue;
-            for (int k = j+1; k <= min(j+grid[i][j], n-1); k++) {
-                if (distance[i][k] > d+1) {
-                    distance[i][k] = d+1;
-                    pq.push({d+1,{ i, k}});
-                    if (i == m-1 && k == n-1) {
-                        return (int)d+1;
-                    }
+          priority_queue<pair<long long,pair< int, int>>, vector<pair<long long,pair< int, int>>>, greater<pair<long long,pair< int, int>>>> pq;
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        pq.push({1,{0,0}});
+      
+        while(!pq.empty()){
+            auto it=pq.top(); pq.pop();
+            int a=it.first,b=it.second.first,c=it.second.second;
+            if(b==n-1 && c==m-1) return a;
+            if(grid[b][c]==0) continue;
+            for(int i=c+1;i<m && i<=grid[b][c]+c;i++){
+                if(!visited[b][i]){
+                     visited[b][i]=1;
+                    if(b==n-1 && i==m-1)
+                        return a+1;
+                    pq.push({a+1,{b,i}});
                 }
             }
-            for (int k = i+1; k <= min(i+grid[i][j], m-1); k++) {
-                if (distance[k][j] > d+1) {
-                    distance[k][j] = d+1;
-                    pq.push({d+1, {k, j}});
-                    if (k == m-1 && j == n-1) {
-                        return (int)d+1;
-                    }
+            for(int i=b+1;i<n && i<=grid[b][c]+b;i++){
+                if(!visited[i][c]){
+                     visited[i][c]=1;
+                     if(i==n-1 && c==m-1)
+                        return a+1;
+                    pq.push({a+1,{i,c}});
                 }
             }
         }
