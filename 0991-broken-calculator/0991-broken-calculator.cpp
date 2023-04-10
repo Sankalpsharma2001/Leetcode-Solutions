dcc50b7f76401698edc51fe5bf514ece3cc100b5
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-04-10 11:53:17 +0530

    Time: 1233 ms (5.50%), Space: 6 MB (8.26%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int ans=0;
+        while(startValue!=target)
+        {
+            if(target%2==0 && target>startValue)
+            {
+                ans++;
+                target/=2;
+            }
+            else
+            {
+                target++;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
