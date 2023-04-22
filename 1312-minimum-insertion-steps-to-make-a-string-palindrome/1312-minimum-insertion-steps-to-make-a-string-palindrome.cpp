ae2a7c55b968339499652d170da3dbc3377e7e56
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-04-22 10:30:08 +0530

    Time: 85 ms (49.08%), Space: 28.6 MB (24.45%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -13,11 +13,32 @@ public:
         return dp[n][m]=max({lcs(s,r,n-1,m,dp),lcs(s,r,n,m-1,dp)});
         
     }
+    int lcs(string s, string r, vector<vector<int>>&dp){
+        int n = s.size();
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n; j++){
+                if(i == 0 || j == 0)
+                dp[i][j] = 0;
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i-1] == r[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
     int minInsertions(string &s) {
         string r=s;
         reverse(r.begin(),r.end());
         vector<vector<int>> dp(s.size()+1,vector<int>(r.size()+1,-1));
-        return s.size()-lcs(s,r,s.size(),r.size(),dp);
-        
+        // return s.size()-lcs(s,r,s.size(),r.size(),dp);
+         return s.size()-lcs(s,r,dp);
+       
     }
 };
