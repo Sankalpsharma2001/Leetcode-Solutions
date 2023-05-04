5a02261861f2b6ce1f58394783444fcf875cbefb
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-05-04 16:34:19 +0530

    Time: 169 ms (20.20%), Space: 13 MB (96.32%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -25,13 +25,32 @@ public:
             return b;
         }
     }
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
          if(lists.size()==0)
             return NULL;
         ListNode *head=lists[0];
         for(int i=1;i<lists.size();i++)
         {
-            head=mergeLists(head,lists[i]);
+            // head=mergeLists(head,lists[i]);
+            head=mergeTwoLists(head,lists[i]);
         }
         return head;
     }
