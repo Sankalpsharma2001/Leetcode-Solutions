f7291abc55c9a02be4b00842c434aa881cfb2d66
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-05-13 08:46:54 +0530

    Time: 20 ms (42.12%), Space: 9.3 MB (92.26%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -14,12 +14,28 @@ public:
         return dp[target]=s%mod;
     }
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<ll> dp(high+5,-1);
-        ll ans=0;
-        for(int i=low;i<=high;i++)
-        {
-            ans=(ans%mod+count(i,zero,one,dp)%mod)%mod;
+        // vector<ll> dp(high+5,-1);
+        // ll ans=0;
+        // for(int i=low;i<=high;i++)
+        // {
+        //     ans=(ans%mod+count(i,zero,one,dp)%mod)%mod;
+        // }
+        // return ans%mod;
+        // Time Complexity is O(N)
+         vector<int> dp(high+1, 0);
+        dp[0]=1;
+        int mod= 1e9+7;
+        for(int i=1; i<=high; i++){
+            if( i-zero>=0)
+                dp[i] = (dp[i] + dp[i-zero])%mod;
+            if( i-one>=0)
+                dp[i]= (dp[i] + dp[i-one])%mod;
+
         }
-        return ans%mod;
+        int ans=0;
+        for(int i=low; i<=high; i++)
+            ans=(ans + dp[i])%mod;
+   
+        return ans;
     }
 };
