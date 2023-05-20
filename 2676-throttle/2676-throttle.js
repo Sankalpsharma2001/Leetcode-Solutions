539c6271b0e4478c77d5e24714a7b1449bd5a103
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-05-20 08:27:15 +0530

    Time: 61 ms (64.69%), Space: 42.7 MB (5.94%) - LeetHub

--- 2676-throttle/2676-throttle.js
+++ 2676-throttle/2676-throttle.js
@@ -4,26 +4,24 @@
  * @return {Function}
  */
 var throttle = function(fn, t) {
-    let timeId=null;
-    let storedArgs=null;
-
-    function callWithStoredArgs(){
-        if(storedArgs){
-            fn.apply(null,storedArgs);
-            storedArgs=null;
-            timeId=setTimeout(callWithStoredArgs,t);
-        } else{
-            timeId=null;
-        }
-    }
-  return function(...args) {
-      if(!timeId){
-          fn.apply(null,args);
-          timeId=setTimeout(callWithStoredArgs,t);
-      }else{
-          storedArgs=args;
+  let cache;
+  let pending = false;
+  let res = (...args) => {
+      if(!pending){
+          cache = undefined;
+          pending = true;
+          setTimeout(()=>{
+              pending = false;
+              if(cache!==undefined){
+                  res(...cache);
+              }
+          },t);
+          fn(...args);
+      } else {
+          cache = args;
       }
   }
+  return res;
 };
 
 
