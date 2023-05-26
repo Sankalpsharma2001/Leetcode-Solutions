9dade8c38b963f723cf6e4b90da347d6899885b8
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-05-26 14:46:38 +0530

    Time: 300 ms (34.44%), Space: 132.2 MB (71.97%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int whoWin(int i,vector<int> &s,vector<int> &dp)
-    {
-        if(i>=s.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int a=INT_MIN,b=INT_MIN,c=INT_MIN;
-        a=s[i]-whoWin(i+1,s,dp);
-        if(i+1<s.size())
-        b=s[i]+s[i+1]-whoWin(i+2,s,dp);
-        if(i+2<s.size())
-        c=s[i]+s[i+1]+s[i+2]-whoWin(i+3,s,dp);
-        
-        return dp[i]=max({a,b,c});
-        
-    }
-    string stoneGameIII(vector<int>& stoneValue) {
-        int n=stoneValue.size();
-        vector<int> dp(stoneValue.size()+1,-1);
-        int res=whoWin(0,stoneValue,dp);
+    
+    string stoneGameIII(vector<int>& s) {
+        int n=s.size();
+        vector<int> dp(n+1,0);
+        int i=n-1;
+        while(i>=0)
+        {
+            int a,b,c;
+            a=b=c=INT_MIN;
+            a=s[i]-dp[i+1];
+             if(i+1<s.size())
+            b=s[i]+s[i+1]-dp[i+2];
+             if(i+2<s.size())
+            c=s[i]+s[i+1]+s[i+2]-dp[i+3];
+             dp[i]=max({a,b,c});
+            i--;
+        }
+        int res=dp[0];
         if (res>0)
             return "Alice";
         if(res==0) return "Tie";
