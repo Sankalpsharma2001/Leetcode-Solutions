d0ef57af0721d10cfbddd176e78d6a54b9ba5280
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-05-31 09:28:26 +0530

    Time: 169 ms (34.59%), Space: 57.2 MB (93.51%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,6 +1,7 @@
+#define ll long long
 class UndergroundSystem {
 public:
-    map<pair<string,string>,vector<int>> avg;
+    map<pair<string,string>,pair<ll,ll>> avg;
     map<int,pair<string,int>> travel;
     UndergroundSystem() {
         
@@ -12,15 +13,21 @@ public:
     
     void checkOut(int id, string stationName, int t) {
         auto p=travel[id];
-        int time=t-p.second;
-        avg[{p.first,stationName}].push_back(time);
+        ll time=t-p.second;
+        if(avg.find({p.first,stationName})==avg.end())
+        {
+            avg[{p.first,stationName}]=make_pair(time,1);
+            return;
+        }
+        auto temp=avg[{p.first,stationName}];
+        avg[{p.first,stationName}]=make_pair(temp.first+time,temp.second+1);
     }
     
     double getAverageTime(string startStation, string endStation) {
         double ans=0;
-        vector<int> v=avg[{startStation,endStation}];
-        for(auto &it:v) ans+=it;
-        return ans/v.size();
+        auto v=avg[{startStation,endStation}];
+        
+        return (double)v.first/v.second;
     }
 };
 
