cab6339e895c5d12c2d8d102e87d4847aece8d5d
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-03 09:05:10 +0530

    Time: 68 ms (19.99%), Space: 41.9 MB (50.77%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,15 +5,13 @@
  */
 var timeLimit = function(fn, t) {
 	return async function(...args) {
-        const f = fn(...args);
-        const pr = new Promise((res, rej) => {
-            setTimeout(() => rej("Time Limit Exceeded"), t);
-        })
-        return Promise.race([f, pr]);
+        return new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => {
+                reject('Time Limit Exceeded'); }, t);
+            fn(...args).then(resolve).catch(reject).finally(() => clearTimeout(timeout));
+        });
     }
 };
-
-
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
