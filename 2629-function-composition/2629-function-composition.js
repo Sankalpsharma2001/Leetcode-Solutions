ebeeecbabed47540454c932d7a2dfb623fca931e
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-03 09:00:26 +0530

    Time: 73 ms (52.74%), Space: 43.2 MB (66.87%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,13 +3,10 @@
  * @return {Function}
  */
 var compose = function(functions) {
-	return function(x) {
-        if(functions.length===0) return x;
-       
-        for(let i=functions.length-1;i>=0;i--)
-            {
-                x=functions[i](x);
-            }
+		return function(x) {
+        functions?.reverse().forEach(fnc => {
+            x = fnc(x);
+        })
         return x;
     }
 };
