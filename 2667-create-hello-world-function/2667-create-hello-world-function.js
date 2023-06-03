470d48801681281d410d1042eb28dd8f0b730078
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-03 08:55:46 +0530

    Time: 63 ms (18.85%), Space: 42.2 MB (9.19%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,12 +1,13 @@
 /**
  * @return {Function}
  */
+ var hello = "Hello World"
 var createHelloWorld = function() {
-    return function(...args) {
-        return "Hello World";
-    }
-};
 
+    return function() {
+        
+    return hello = "Hello World"; }
+};
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
