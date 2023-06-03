4b88e21f1e068f8f20448098e33669d94c98e87a
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-03 08:58:38 +0530

    Time: 61 ms (60.76%), Space: 42.2 MB (82.44%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,10 +5,10 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let ans=init;
-    for(let i=0;i<nums.length;i++)
-        {
-            ans=fn(ans,nums[i]);
-        }
-    return ans;
+    let res = init;
+
+    for(let i = 0; i < nums.length; i++) {
+        res = fn(res, nums[i])
+    }
+    return res;
 };
