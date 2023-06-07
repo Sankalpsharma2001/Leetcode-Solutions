a4dc59ad72d5f5e40fb18a38b8d239fc3fbf1e65
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-07 13:06:32 +0530

    Time: 28 ms (97.88%), Space: 21.4 MB (11.65%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -43,17 +43,46 @@ vector<int> calculateZ(string &s)
     return z;   
 
 }
+    vector<int> prefix_function(string s)
+{
+    vector<int> lps(s.size(),0);
+    lps[0]=0;
+    int len=0,i=1;
+    while(i<s.size())
+    {
+       if(s[i]==s[len])
+       {
+        lps[i]=len+1;
+        len++,i++;
+       }
+       else
+       {
+        if(len!=0)
+        {
+            len=lps[len-1];
+        }
+        else
+        {
+            lps[i]=0;
+            i++;
+        }
+       }
+    }
+        return lps;
+    }
     string longestPrefix(string s) {
         
-  vector<int> z=calculateZ(s);
-      int mx=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(z[i]==s.size()-i)
-            {
-                mx=max(mx,(int)s.size()-i);
-            }
-        }
+  // vector<int> z=calculateZ(s);
+      // int mx=0;
+      //   for(int i=0;i<s.size();i++)
+      //   {
+      //       if(z[i]==s.size()-i)
+      //       {
+      //           mx=max(mx,(int)s.size()-i);
+      //       }
+      //   }
+        vector<int> pre=prefix_function(s);
+        int mx=pre[s.size()-1];
         return s.substr(0,mx);
     }
 };
