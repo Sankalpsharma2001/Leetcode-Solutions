aac6f4a6fbeaa15fa34e0f9e8de610b64e5b45c7
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-13 09:10:49 +0530

    Time: 113 ms (59.09%), Space: 32.7 MB (47.26%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<int>> r;
-        for(int i=0;i<n;i++)
+         int ans = 0;
+        map<vector<int>, int> mp;
+        for (int i = 0; i < grid.size(); i++)
+            mp[grid[i]]++;
+        
+        for (int i = 0; i < grid[0].size(); i++)
         {
             vector<int> v;
-            for(int j=0;j<n;j++)
-            {
-                v.push_back(grid[i][j]);
-            }
-           r.push_back(v);
-        }
-        vector<vector<int>> c;
-        for(int i=0;i<n;i++)
-        {
-            vector<int> v;
-            for(int j=0;j<n;j++)
-            {
+            for (int j = 0; j < grid.size(); j++)
                 v.push_back(grid[j][i]);
-            }
-            c.push_back(v);
-        }
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(r[i]==c[j])
-                {
-                    ans++;
-                }
-            }
+      
+            ans += mp[v];
         }
         return ans;
-        
     }
 };
