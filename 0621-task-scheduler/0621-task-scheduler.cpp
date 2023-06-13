f64468ed8fc40e0d875d37fe592a95d29c097e33
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-13 15:49:27 +0530

    Time: 81 ms (60.45%), Space: 34.4 MB (84.43%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-      unordered_map<int,int> m;
-        for(auto &it:tasks)
+        unordered_map<char,int>mp;
+        int count = 0;
+        for(auto& e : tasks)
         {
-            m[it]++;
+            mp[e]++;
+            count = max(count, mp[e]);
         }
-        priority_queue<int> pq;
-        for(auto &it:m)
-        {
-            pq.push(it.second);
-        }
-        int cycle=n+1;
-        int alltime=0;
         
-        while(!pq.empty())
-        {
-            int time=0;
-            vector<int> tmp;
-            for (int i = 0; i < cycle; i++) {
-                if (!pq.empty()) {
-                    tmp.push_back(pq.top());
-                    pq.pop();
-                    time++;
-                }
-            }
-            for (int cnt : tmp) {
-                if (--cnt) {
-                    pq.push(cnt);
-                }
-            }
-            alltime += !pq.empty() ? cycle : time;
-            
-        }
-     return alltime;
+        int ans = (count-1)*(n+1);
+        for(auto &e : mp) if(e.second == count) ans++;
+        return max((int)tasks.size(), ans);
+//         First count the number of occurrences of each element.
+// Let the max frequency seen be M for element E
+// We can schedule the first M-1 occurrences of E, each E will be followed by at least N CPU cycles in between successive schedules of E
+      // ex A _ _ A _ _ (count-1)*(n+1)  
+        
+// Total CPU cycles after scheduling M-1 occurrences of E = (M-1) * (N + 1) // 1 comes for the CPU cycle for E itself
+// Now schedule the final round of tasks. We will need at least 1 CPU cycle of the last occurrence of E. If there are multiple tasks with frequency M, they will all need 1 more cycle.
+        // ex AB_ AB_ AB
+// Run through the frequency dictionary and for every element which has frequency == M, add 1 cycle to result.
+// If we have more number of tasks than the max slots we need as computed above we will return the length of the tasks array as we need at least those many CPU cycles
     }
 };
