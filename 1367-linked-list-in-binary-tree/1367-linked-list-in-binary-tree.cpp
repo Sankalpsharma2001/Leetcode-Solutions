171389f9dc55a597dcd801dd093534a166b89460
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-16 16:27:18 +0530

    Time: 39 ms (57.27%), Space: 32.1 MB (46.67%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -21,30 +21,34 @@
  */
 class Solution {
 public:
-     map<pair<ListNode*, TreeNode*>, bool> m;
-    bool check(ListNode* head, TreeNode* root, ListNode* ihead){
-        if(head==NULL){
-            return true;
-        }
-        if(root==NULL){
-            return false;
-        }
-        if(m.count({head, root})!=0){
-            return m[{head, root}];
-        }
-        bool o1=false,o2=false;
-        if(ihead->val==root->val){
-            o1 = check(ihead->next, root->left, ihead) || check(ihead->next, root->right, ihead);
-        }
-        else{
-            o1 = check(ihead, root->left, ihead) || check(ihead, root->right, ihead);
-        }
-        if(head->val==root->val){
-            o2 = check(head->next, root->left, ihead) || check(head->next, root->right, ihead);
-        }
-        return m[{head, root}]=o1||o2;
-    }
     bool isSubPath(ListNode* head, TreeNode* root) {
-        return check(head, root, head);
+        return dfs(head, root, false);
     }
-};  
+    bool dfs(ListNode* head, TreeNode* root, bool head_match_last) {
+        // use up all required elements in head
+        if (head == nullptr) {
+            return head_match_last;
+        }
+        // remaining elements in head, but we have reached the leaf
+        if (root == nullptr) return false; 
+        if (head_match_last) {
+            if (head->val != root->val) return false;
+            if (dfs(head->next, root->left, true) || dfs(head->next, root->right, true)) {
+                return true;
+            }
+        }
+        else {
+            if (head->val == root->val) {
+                // choose to match current element
+                if (dfs(head->next, root->left, true) || dfs(head->next, root->right, true)) {
+                    return true;
+                }
+            }
+            // continue to not match current element
+            if (dfs(head, root->left, false) || dfs(head, root->right, false)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
