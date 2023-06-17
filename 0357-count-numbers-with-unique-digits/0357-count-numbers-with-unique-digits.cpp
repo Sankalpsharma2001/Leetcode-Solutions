82866081a3970ecb7c8833aca847cb60218c19d7
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-17 12:18:47 +0530

    Time: 0 ms (100.00%), Space: 7 MB (6.40%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[10][10][2][1<<10];
+    int digitDP(string &s,int index,int isRepeated,int flag,int mask)
+    {
+        if(index==s.size())
+        {
+            return isRepeated==0;
+        }
+        if(dp[index][isRepeated][flag][mask]!=-1) return dp[index][isRepeated][flag][mask];
+        int range=(flag)?s[index]-'0':9;
+        int ans=0;
+        for(int i=0;i<=range;i++)
+        {
+            int newFlag=(flag && i==s[index]-'0')?1:0;
+            if(i==0 && mask==0) 
+            ans+=digitDP(s,index+1,0,newFlag,mask);
+            else if(mask&(1<<i))
+            ans+=digitDP(s,index+1,1,newFlag,mask);
+            else
+            ans+=digitDP(s,index+1,isRepeated,newFlag,mask|(1<<i));    
+        }
+        return dp[index][isRepeated][flag][mask]=ans;
+    }
+    int countNumbersWithUniqueDigits(int n) {
+         memset(dp,-1,sizeof(dp));
+       
+        n=pow(10,n);
+        n--;
+        string s=to_string(n);
+        return digitDP(s,0,0,1,0);
+    }
+};
