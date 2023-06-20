e894155604b7e1cfe009178d9383337a58e306f5
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-20 08:15:23 +0530

    Time: 258 ms (58.32%), Space: 130 MB (66.09%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -5,13 +5,16 @@ public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int> ans(n,-1);
-        vector<ll> pre(n+1,0);
+       ll sum=0;
+      int j=0;
         for(int i=0;i<n;i++)
-            pre[i+1]=pre[i]+nums[i];
-        for(int i=k;i<n-k;i++)
         {
-            ll avg=(pre[i+k+1]-pre[i-k]);
-            ans[i]=avg/(2*k+1);
+            if(i<2*k) {sum+=nums[i];
+            continue;}
+            sum+=nums[i];
+            ll avg=sum;
+            ans[i-k]=avg/(2*k+1);
+            sum-=nums[j++];
         }
         return ans;
         
