6d6664bc1b21dc2f0662f3ea2a49b3796959ee3f
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-21 09:25:12 +0530

    Time: 142 ms (18.61%), Space: 38.6 MB (55.11%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,35 +1,39 @@
-#define ll long long
 class Solution {
 public:
-    long long minCost(vector<int>& nums, vector<int>& cost) {
-        int n=nums.size();
-        vector<pair<int,int>> p;
+    long long findSum(vector<int>& nums, vector<int>& cost,long long mid)
+    {
+        long long sum=0;
         for(int i=0;i<nums.size();i++)
         {
-            p.push_back({nums[i],cost[i]});
+            long long x=abs(nums[i]-mid);
+            sum+=(x*cost[i]);
         }
-        sort(p.begin(),p.end());
-        vector<ll> pre(n,0);
-        pre[0]=p[0].second;
-        for(int i=1;i<n;i++)
+        return sum;
+    }
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        long long low=0,high=1e6;
+       
+        while(low<=high)
         {
-            pre[i]=pre[i-1]+p[i].second;
+            long long mid=low+(high-low)/2;
+            long long a=findSum(nums,cost,mid);
+            long long b=findSum(nums,cost,mid-1);
+            long long c=findSum(nums,cost,mid+1);
+           
+            if(a<=b && a<=c)
+            {
+                return a;
+            }
+            if(a>c)
+            {
+                
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
         }
-        ll c=0;
-        for(int i=1;i<n;i++)
-        {
-            c+=1ll*(p[i].second*1ll*(p[i].first-p[0].first));
-        }
-        ll ans=c;
-        for(int i=1;i<n;i++)
-        {
-            ll gap=p[i].first-p[i-1].first;
-            c+=1ll*(pre[i-1]*gap);
-            c-=1ll*(pre[n-1]-pre[i-1])*gap;
-            ans=min(ans,c);
-        }
-        return ans;
-        
-        
+        return 0;
     }
 };
