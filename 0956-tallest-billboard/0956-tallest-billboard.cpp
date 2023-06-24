c6488780624a90916176d4849ee30438555274e5
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-06-24 11:46:24 +0530

    Time: 105 ms (56.39%), Space: 8.7 MB (85.46%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    map<pair<int,int>,int> dp;
+   int dp[10005][21];
     int solve(vector<int> &rods,int l1,int index)
     {
         if(index==rods.size())
         {
             if(l1==0)
-                return 0;
-            return  dp[{l1,index}]=INT_MIN;
+                return  dp[l1+5000][index]=0;
+            return dp[l1+5000][index]=INT_MIN;
         }
-        if(dp.count({l1,index})) return dp[{l1,index}];
+        if(dp[l1+5000][index]!=-1) return dp[l1+5000][index];
         int a=rods[index]+solve(rods,l1+rods[index],index+1);
         int b=solve(rods,l1-rods[index],index+1);
         int c=solve(rods,l1,index+1);
-        return dp[{l1,index}]=max({a,b,c});
+        return dp[l1+5000][index]=max({a,b,c});
     }
     int tallestBillboard(vector<int>& rods) {
       int n=rods.size();
-      
+      memset(dp,-1,sizeof(dp));
         return solve(rods,0,0);
     }
 };
