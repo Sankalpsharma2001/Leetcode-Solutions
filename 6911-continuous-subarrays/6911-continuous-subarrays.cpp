1e40956e39f532021424509e99a068a208eb0b2f
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-07-02 10:18:44 +0530

    Time: 170 ms (92.31%), Space: 107.1 MB (84.62%) - LeetHub

--- 6911-continuous-subarrays/6911-continuous-subarrays.cpp
+++ 6911-continuous-subarrays/6911-continuous-subarrays.cpp
@@ -4,21 +4,19 @@ public:
     long long continuousSubarrays(vector<int>& nums) {
         int n=nums.size();
         long long ans=0;
-      multiset<int> a;
-        int i=0,j=0;
-      multiset<int,greater<int>> b;
-        while(j<n)
+int i=0,j=0;
+        map<ll,ll> mp;
+         while(j<n)
         {
-            a.insert(nums[j]);
-            b.insert(nums[j]);
-            while(*b.begin()-*a.begin()>2)
-            {
-                auto it=a.find(nums[i]);
-                auto it1=b.find(nums[i]);
-                a.erase(it);
-                b.erase(it1);
-                i++;
-            }
+             mp[nums[j]]++;
+             while(mp.size()>0 && mp.rbegin()->first-mp.begin()->first>2)
+             {
+                 mp[nums[i]]--;
+                 if(mp[nums[i]]==0)
+                     mp.erase(nums[i]);
+                 i++;
+             }
+           
             ans+=j-i+1;
             j++;
          }
