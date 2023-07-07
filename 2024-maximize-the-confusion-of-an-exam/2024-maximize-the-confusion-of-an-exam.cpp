1cbce73317080b71628a2f84599089087e936914
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-07-07 09:26:07 +0530

    Time: 81 ms (18.94%), Space: 10.1 MB (59.24%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    bool check(string &s,int k,int mid)
-    {
-        int n=s.size();
-        int i=0,j=0;
-        unordered_map<char,int> mp;
-        for(int i=0;i<mid;i++)
-            mp[s[i]]++;
-         if(min(mp['T'],mp['F'])<=k) return true;
-        j=mid;
-        while(j<n)
-        {
-           mp[s[j]]++;
-            mp[s[i]]--;
-            if(min(mp['T'],mp['F'])<=k) return true;
-            i++;
-            j++;
-        }
-        return false;
-    }
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int n=answerKey.size();
-        int low=k,high=n,ans=k;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(check(answerKey,k,mid))
-            {
-                ans=mid;
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
+           int maxSize = 0;
+        unordered_map<char, int> count;
+        
+        for (int right = 0; right < answerKey.size(); right++) {
+            count[answerKey[right]]++;
+            int minor = min(count['T'], count['F']);
+            
+            if (minor <= k) {
+                maxSize++;
+            } else {
+                count[answerKey[right - maxSize]]--;
             }
         }
-        return ans;
+
+        return maxSize;
+    
     }
 };
