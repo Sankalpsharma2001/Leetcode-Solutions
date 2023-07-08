86942b43c41015b4cf66a58b268c0e756feb69f8
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-07-08 22:41:36 +0530

    Time: 9 ms (41.18%), Space: 6 MB (94.12%) - LeetHub

--- 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
@@ -34,10 +34,11 @@ public:
         }
         return (isFive(ans));
     }
+    int dp[16];
     int solve(int i,int n,string &s)
     {
         if(i>=n) return 0;
-        
+        if(dp[i]!=-1) return dp[n];
         int ans=1e9;
         
         for(int idx=i;idx<n;idx++)
@@ -48,10 +49,11 @@ public:
             ans=min(ans,t);
             }
         }
-        return ans;
+        return dp[n]=ans;
     }
     int minimumBeautifulSubstrings(string s) {
         int n=s.size();
+        memset(dp,-1,sizeof(dp));
         int ans=solve(0,n,s);
         return ans==1e9?-1:ans;
     }
