51af673cea8be137bb96bfa975a33709a8f4ede2
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-07-15 10:03:18 +0530

    Time: 326 ms (74.41%), Space: 72.7 MB (74.56%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+    int bs(int st,vector<vector<int>> &e,int val)
+    {
+        int low=st,high=e.size()-1,ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(e[mid][0]>val)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
     int dfs(int i,int k,vector<vector<int>> &e,vector<vector<int>> &dp)
     {
         int n=e.size();
-        if(i==n) return 0;
-        if(k==0) return 0;
+        if(i==n || i==-1 || k==0) return 0;
+        
         if(dp[i][k]!=-1) return dp[i][k];
-        int idx;
-        for(idx=i+1;idx<n;idx++)
-        {
-            if(e[i][1]<e[idx][0]) break;
-        }
+        int idx = bs(i+1,e,e[i][1]);
+        // int idx;
+        // for(idx=i+1;idx<n;idx++)
+        // {
+        //     if(e[i][1]<e[idx][0]) break;
+        // }
         return dp[i][k]=max(e[i][2]+dfs(idx,k-1,e,dp),dfs(i+1,k,e,dp));
     }
     int maxValue(vector<vector<int>>& events, int k) {
