0fbb19744dc8f56960286e28f42e7df9324ca884
Sankalp Sharma <75837192+Sankalpsharma2001@users.noreply.github.com>
2023-10-01 10:44:31 +0530

    Time: 1671 ms (6.86%), Space: 422.8 MB (5.29%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int ans=0;
+    int solve(string s,string t,int n,int m)
+    {
+        if(m>n) return 0;
+       unsigned long long int dp[1001][1001];
+        for(int i=0;i<=n;i++)
+        {
+            dp[0][i]=1;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {    
+                 if(t[i-1]==s[j-1])
+                {
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    int solveRecursion(string s,string t,int n,int m, vector<vector<int>> &dp)
+    {
+        if(n==0 && m==0) return 1;
+        if(m==0) return 1;
+        if(n==0) return 0;
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(s[n-1]==t[m-1])
+            return dp[n][m]=solveRecursion(s,t,n-1,m-1,dp)+solveRecursion(s,t,n-1,m,dp);
+        return dp[n][m]=solveRecursion(s,t,n-1,m,dp);
+            
+        
+    }
+    int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
+       return solveRecursion(s,t,s.size(),t.size(),dp);
+        
+    }
+};
